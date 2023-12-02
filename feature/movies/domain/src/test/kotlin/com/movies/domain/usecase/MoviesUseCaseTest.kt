b9614e71cf5151754022585096f2ec3704873719
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-02 16:41:33 +0200

    Updating test case for MoviesUseCase Adding Mockk and Coroutines dependencies

--- feature/movies/domain/src/test/kotlin/com/movies/domain/usecase/MoviesUseCaseTest.kt
+++ feature/movies/domain/src/test/kotlin/com/movies/domain/usecase/MoviesUseCaseTest.kt
@@ -1,17 +1,30 @@
 package com.movies.domain.usecase
 
 import com.movies.domain.entity.Movie
+import com.movies.domain.repository.MoviesRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class MoviesUseCaseTest {
 
-    private val useCase = MoviesUseCase()
+    private val mockRepository: MoviesRepository = mockk()
+    private val useCase = MoviesUseCase(mockRepository)
 
     @Test
     fun `useCase is returning movies`() {
-        val movies = useCase.invoke()
-        assertEquals(emptyList<Movie>(), movies)
+        coEvery { mockRepository.getMovies() } returns listOf(
+            Movie(),
+            Movie()
+        )
+        runBlocking {
+            val movies = useCase.invoke()
+            assertEquals(2, movies.size)
+            coVerify { mockRepository.getMovies() }
+        }
     }
 
 }
