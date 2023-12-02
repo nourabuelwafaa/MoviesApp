33816b3662a65bb64e2247c1a6c55041b29ee7d9
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-02 16:26:44 +0200

    Creating invoke() function to return listOf movies useCase.

--- feature/movies/domain/src/main/kotlin/com/movies/domain/usecase/MoviesUseCase.kt
+++ feature/movies/domain/src/main/kotlin/com/movies/domain/usecase/MoviesUseCase.kt
@@ -1,4 +1,11 @@
 package com.movies.domain.usecase
 
+import com.movies.domain.entity.Movie
+
 class MoviesUseCase {
+
+    fun invoke(): List<Movie> {
+        return emptyList()
+    }
+
 }
--- feature/movies/domain/src/test/kotlin/com/movies/domain/usecase/MoviesUseCaseTest.kt
+++ feature/movies/domain/src/test/kotlin/com/movies/domain/usecase/MoviesUseCaseTest.kt
@@ -1,12 +1,17 @@
 package com.movies.domain.usecase
 
+import com.movies.domain.entity.Movie
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class MoviesUseCaseTest {
 
-    @Test
-    fun test() {
+    private val useCase = MoviesUseCase()
 
+    @Test
+    fun `useCase is returning movies`() {
+        val movies = useCase.invoke()
+        assertEquals(emptyList<Movie>(), movies)
     }
 
 }
