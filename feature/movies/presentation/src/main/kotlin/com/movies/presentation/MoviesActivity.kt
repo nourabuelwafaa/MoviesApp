9bf43c56850564690c15214f0cc3af8d4d52db45
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-03 08:10:56 +0200

    Creating single movie view.

--- feature/movies/presentation/src/main/kotlin/com/movies/presentation/MoviesActivity.kt
+++ feature/movies/presentation/src/main/kotlin/com/movies/presentation/MoviesActivity.kt
@@ -2,10 +2,12 @@ package com.movies.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.movies.feature.movies.presentation.R
 
 class MoviesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies)
     }
 }
