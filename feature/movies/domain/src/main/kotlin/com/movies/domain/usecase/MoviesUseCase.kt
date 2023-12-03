d8b566a680d23d9f257e3bb83e644bb9407a488e
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-03 22:55:03 +0200

    Adding Inject annotation dependency and using it in domain layer.

--- feature/movies/domain/src/main/kotlin/com/movies/domain/usecase/MoviesUseCase.kt
+++ feature/movies/domain/src/main/kotlin/com/movies/domain/usecase/MoviesUseCase.kt
@@ -2,8 +2,9 @@ package com.movies.domain.usecase
 
 import com.movies.domain.entity.Movie
 import com.movies.domain.repository.MoviesRepository
+import javax.inject.Inject
 
-class MoviesUseCase(private val repository: MoviesRepository) {
+class MoviesUseCase @Inject constructor(private val repository: MoviesRepository) {
 
     suspend fun invoke(): Result<List<Movie>> {
         return repository.getMovies()
