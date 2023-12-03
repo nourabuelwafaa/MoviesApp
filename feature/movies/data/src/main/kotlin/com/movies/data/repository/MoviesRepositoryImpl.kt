7cd5b4b605239d05df64f94b8498a6b196684fab
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-03 07:16:10 +0200

    Parsing api response and updating unit tests.

--- feature/movies/data/src/main/kotlin/com/movies/data/repository/MoviesRepositoryImpl.kt
+++ feature/movies/data/src/main/kotlin/com/movies/data/repository/MoviesRepositoryImpl.kt
@@ -8,6 +8,15 @@ class MoviesRepositoryImpl(
     private val api: MoviesApi
 ) : MoviesRepository {
     override suspend fun getMovies(): Result<List<Movie>> {
-        return Result.success(emptyList())
+        return try {
+            val response = api.movies()
+            if (response.isSuccessful && response.body() != null) {
+                Result.success(emptyList())
+            } else {
+                Result.failure(Exception(response.message()))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 }
--- feature/movies/data/src/test/kotlin/com/movies/data/repository/MoviesRepositoryImplTest.kt
+++ feature/movies/data/src/test/kotlin/com/movies/data/repository/MoviesRepositoryImplTest.kt
@@ -1,18 +1,50 @@
 package com.movies.data.repository
 
+import com.movies.data.dto.MoviesResponse
+import com.movies.data.remote.MoviesApi
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
+import okhttp3.ResponseBody
 import org.junit.Assert
 import org.junit.Test
+import retrofit2.Response
 
 class MoviesRepositoryImplTest {
+    private val mockApi = mockk<MoviesApi>()
+    private val repository = MoviesRepositoryImpl(mockApi)
 
-    private val repository = MoviesRepositoryImpl()
 
     @Test
     fun `repository is returning items`() {
+        coEvery { mockApi.movies() } returns
+                Response.success(MoviesResponse(emptyList()))
         runBlocking {
             val result = repository.getMovies()
-            result.onSuccess { Assert.assertEquals(0, it.size) }
+            Assert.assertEquals(0, result.getOrNull()!!.size)
+        }
+    }
+
+    @Test
+    fun `repository is returning failure in case api failed`() {
+        coEvery { mockApi.movies() } returns
+                Response.error(
+                    500,
+                    ResponseBody.create(null, "")
+                )
+        runBlocking {
+            val result = repository.getMovies()
+            Assert.assertEquals(result.isFailure, true)
+        }
+    }
+
+
+    @Test
+    fun `repository is returning failure in case api throw exception`() {
+        coEvery { mockApi.movies() } throws Exception()
+        runBlocking {
+            val result = repository.getMovies()
+            Assert.assertEquals(result.isFailure, true)
         }
     }
 
