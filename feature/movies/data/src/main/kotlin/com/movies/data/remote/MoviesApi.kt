3d1bcf931dd52ac75c8351abb9dc48d9f61e35f6
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-04 08:56:49 +0200

    Updating API endpoint.

--- feature/movies/data/src/main/kotlin/com/movies/data/remote/MoviesApi.kt
+++ feature/movies/data/src/main/kotlin/com/movies/data/remote/MoviesApi.kt
@@ -2,7 +2,10 @@ package com.movies.data.remote
 
 import com.movies.data.dto.MoviesResponse
 import retrofit2.Response
+import retrofit2.http.GET
 
 interface MoviesApi {
+    // TODO: extract api key
+    @GET("3/discover/movie?api_key=c9856d0cb57c3f14bf75bdc6c063b8f3")
     suspend fun movies(): Response<MoviesResponse>
 }
