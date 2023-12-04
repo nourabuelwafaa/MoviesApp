2b0a6c43145b5d85f9177a50479a6c6a75b77017
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-04 09:02:42 +0200

    Creating module for mapper.

---
+++ feature/movies/presentation/src/main/kotlin/com/movies/presentation/di/MoviesViewMapperModule.kt
@@ -0,0 +1,15 @@
+import com.movies.presentation.mapper.MoviesViewMapper
+import com.movies.presentation.mapper.MoviesViewMapperImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface MoviesViewMapperModule {
+    @Binds
+    fun bindsMoviesReposMapper(
+        impl: MoviesViewMapperImpl
+    ): MoviesViewMapper
+}
