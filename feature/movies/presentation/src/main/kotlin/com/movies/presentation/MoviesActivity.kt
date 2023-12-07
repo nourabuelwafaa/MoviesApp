c035c32dc993119498c899d53fc574f9ab102a30
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-07 08:03:30 +0200

    Observing on moviesLiveData from activity.

--- feature/movies/presentation/src/main/kotlin/com/movies/presentation/MoviesActivity.kt
+++ feature/movies/presentation/src/main/kotlin/com/movies/presentation/MoviesActivity.kt
@@ -1,15 +1,24 @@
 package com.movies.presentation
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.movies.feature.movies.presentation.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MoviesActivity : AppCompatActivity() {
+    private val viewModel: MoviesViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies)
+        viewModel.getMovies()
+        viewModel.moviesLiveData.observe(this) {
+            findViewById<RecyclerView>(R.id.moviesRv).adapter =
+                MoviesAdapter(it) {}
+        }
+
     }
 }
