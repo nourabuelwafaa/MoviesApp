197ee9ddedf84fd12e9ed0ecefe60f2fcd97a37a
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-07 09:43:27 +0200

    Making MoviesView parcelable in order to send to through intent.

--- feature/movies/presentation/src/main/kotlin/com/movies/presentation/model/MoviesView.kt
+++ feature/movies/presentation/src/main/kotlin/com/movies/presentation/model/MoviesView.kt
@@ -1,7 +1,36 @@
 package com.movies.presentation.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class MoviesView(
     val title: String,
     val date: String,
-    val imageUrl: String
-)
+    val imageUrl: String,
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(date)
+        parcel.writeString(imageUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MoviesView> {
+        override fun createFromParcel(parcel: Parcel): MoviesView {
+            return MoviesView(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MoviesView?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
