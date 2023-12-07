b426672acb2b74b0b82e023bffb5f1553ab14e36
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-07 09:24:28 +0200

    Endpoint enhancements.

--- feature/movies/data/src/main/kotlin/com/movies/data/remote/MoviesApi.kt
+++ feature/movies/data/src/main/kotlin/com/movies/data/remote/MoviesApi.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 
 interface MoviesApi {
     // TODO: extract api key
-    @GET("3/discover/movie?api_key=c9856d0cb57c3f14bf75bdc6c063b8f3")
+    @GET("3/discover/movie?api_key=c9856d0cb57c3f14bf75bdc6c063b8f3&with_genres=16")
     suspend fun movies(): Response<MoviesResponse>
 }
