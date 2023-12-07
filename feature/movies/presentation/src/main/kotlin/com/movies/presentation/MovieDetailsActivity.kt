d69d8e483dcfa7e012f89957d3f3b7768b571a9a
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-07 09:49:23 +0200

    Parsing passed data in details screen.

--- feature/movies/presentation/src/main/kotlin/com/movies/presentation/MovieDetailsActivity.kt
+++ feature/movies/presentation/src/main/kotlin/com/movies/presentation/MovieDetailsActivity.kt
@@ -3,16 +3,27 @@ package com.movies.presentation
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
 import com.movies.feature.movies.presentation.databinding.ActivityMovieDetailsBinding
+import com.movies.presentation.model.MoviesView
 
 class MovieDetailsActivity :
     AppCompatActivity() {
     private lateinit var binding: ActivityMovieDetailsBinding
 
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMovieDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val movie =
+            intent.getParcelableExtra<MoviesView>(MoviesActivity.MOVIE_DETAILS)
+
+        movie?.let {
+            binding.dateTv.text = it.date
+            binding.titleTv.text = it.title
+            binding.posterIv.load(it.imageUrl)
+        }
     }
 
 }
