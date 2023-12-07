d9d2e15255df3d3a277a97921997628ec27c80da
Nour Ahmed <nourabuelwafaa@gmail.com>
2023-12-07 09:48:38 +0200

    Opening details screen on item click.

--- feature/movies/presentation/src/main/kotlin/com/movies/presentation/MoviesActivity.kt
+++ feature/movies/presentation/src/main/kotlin/com/movies/presentation/MoviesActivity.kt
@@ -1,10 +1,12 @@
 package com.movies.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.movies.feature.movies.presentation.R
+import com.movies.presentation.model.MoviesView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,8 +19,17 @@ class MoviesActivity : AppCompatActivity() {
         viewModel.getMovies()
         viewModel.moviesLiveData.observe(this) {
             findViewById<RecyclerView>(R.id.moviesRv).adapter =
-                MoviesAdapter(it) {}
+                MoviesAdapter(it) { movie -> showDetails(movie) }
         }
+    }
 
+    private fun showDetails(movie: MoviesView) {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        intent.putExtra(MOVIE_DETAILS, movie)
+        startActivity(intent)
+    }
+
+    companion object {
+        const val MOVIE_DETAILS = "movieDetails"
     }
 }
